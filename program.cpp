c2ec08b4ed21bc4dbb336c394ecef7b796d5e395
Timothy Nakev <99489258+MyNamesTim@users.noreply.github.com>
2026-04-16 09:20:12 -0400

    Refactor functions to use references instead of pointers

--- program.cpp
+++ program.cpp
@@ -9,40 +9,41 @@
 #include <random>
 using namespace std;
 
-void lower(string *value) {
-    for(int i=0;i<(*value).length();i++) {
-        (*value)[i]=tolower((*value)[i]);
+void lower(string &value) {
+    for(int i=0;i<value.length();i++) {
+        value[i]=tolower(value[i]);
     }
 }
 
-void mult(int *value) {
-    (*value) *= 2;
+void mult(int &value) {
+    value *= 2;
 }
 
-void rand_mult(int *value) {
+void rand_mult(int &value) {
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> distr(1, 100);
-    (*value) = (*value) + distr(gen);
+    value = value + distr(gen);
 }
 
 int main() {
     string yes;
     cout << "write the value " << endl;
     cin >> yes;
-    lower(&yes);
+    lower(yes);
     cout << yes << endl;
 
     int thing;
     cout << "enter the int " << endl;
     cin >> thing;
-    mult(&thing);
+    mult(thing);
     cout << thing << endl;
 
     int new_thing;
     cout << "enter another int " << endl;
     cin >> new_thing;
-    rand_mult(&new_thing);
+    rand_mult(new_thing);
     cout << new_thing << endl;
 
+    return 0;
 }
